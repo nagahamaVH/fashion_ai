ad700ba2bf2121d3afe293e2a5cba77907812aba
Victor Nagahama <victorh.nagahama@gmail.com>
2021-07-25 10:30:13 -0300

    feat: 🎸 method to predict and draw image

--- engine/src/predict.py
+++ engine/src/predict.py
@@ -4,9 +4,11 @@ from PIL import Image
 import torch
 import torchvision.transforms as T
 from fasterrcnn_model import get_instance_segmentation_model
+from drawing import draw_segm
+from utils import map_labels_colors
 
 
-def predict(image, model_name, device=None):
+def _predict(image, model_name, device=None):
     if device is None:
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -34,21 +36,30 @@ def predict(image, model_name, device=None):
     return info
 
 
+def predict(image, model_name, device=None):
+    if device is None:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    info = _predict(image, model_name, device)
+
+    classes = pd.read_csv(os.path.join("pretrained_models", model_name + ".csv"))
+    mapped_color = map_labels_colors(classes["ClassId"].values)
+    image_proc = draw_segm(image, info, mapped_color)
+
+    return image_proc, info
+
+
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
+    import numpy as np
 
     image_path = "../data/train/0a0f94b4e785bb2326dd1832303ce8de.jpg"
     # image_path = "lixo.jpeg"
     image = Image.open(image_path).convert('RGB')
     image = image.resize((256, 256), resample=Image.BILINEAR)
-    info = predict(image, "test_20epoch")
+    image = np.asarray(image)
+    image_proc, info = predict(image, "model_batch4")
 
-    plt.imshow(image)
+    plt.imshow(image_proc)
     plt.show()
 
     print(info)
-
-    detected_image = Image.fromarray(info['masks'][0, 0])
-    print(info["masks"].shape)
-    plt.imshow(detected_image)
-    plt.show()
